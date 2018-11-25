d5c567c88b1e41eaaed7468d497404621d770b74
Marcelo Barbosa <ma.barbosa@outlook.com>
2018-11-25 12:54:44 -0200

    Últimos Ajustes

---
+++ MaeTerra.Dados/Anuncio/AnuncioDados.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaeTerra.Infraestrutura.EDMX;
+using System.Data.Entity;
+
+namespace MaeTerra.Dados.Anuncios
+{
+    public class AnuncioDados : IAnuncioDados
+    {
+        #region ATRIBUTOS E CONSTRUTOR
+        private UnileverDBEntities db = null;
+
+        /// <summary>
+        /// Metodo contrutor
+        /// </summary>
+        /// <param name="db"></param>
+        public AnuncioDados(UnileverDBEntities _db)
+        {
+            this.db = _db;
+        }
+        #endregion
+
+        #region LOCAL
+        /// <summary>
+        /// Metodo responsavel por buscar todos os anuncios
+        /// </summary>
+        /// <returns></returns> 
+        public IQueryable<Anuncio> BuscaAnuncios()
+        {
+            return db.Anuncios;
+        }
+
+        /// <summary>
+        /// Metodo responsavel por alterar o anuncio 
+        /// </summary>
+        /// <param name="anuncio"></param>
+        public Anuncio AlterarAnuncio(Anuncio anuncio)
+        {
+            db.Entry<Anuncio>(anuncio).State = EntityState.Modified;
+            db.SaveChanges();
+
+            return anuncio;
+        }
+
+        /// <summary>
+        /// Metodo responsavel por salvar o anuncio
+        /// </summary>
+        /// <param name="anuncio"></param>
+        public Anuncio SalvarAnuncio(Anuncio anuncio)
+        {
+            this.db.Anuncios.Add(anuncio);
+            db.SaveChanges();
+
+            return anuncio;
+        }
+        #endregion
+    }
+}
---
+++ MaeTerra.Negocios/Anuncio/IAnuncioNegocio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaeTerra.Negocios.Anuncio
+{
+    public interface IAnuncioNegocios
+    {
+        IList<Anuncio> BuscaAnuncios(int codUsuario);
+        IList<AnuncioHistorico> BuscaAnuncioHistoricos(int codAnuncio);
+    }
+}
